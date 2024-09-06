068725ef0049a47193cdb54c38dd0b5c6cdefe35
BerryRahul <113147217+BerryRahul@users.noreply.github.com>
2024-09-05 23:58:24 -0400

    completed day 15

--- 15 Add Items to Checkbox /script.js
+++ 15 Add Items to Checkbox /script.js
@@ -32,5 +32,15 @@ function populateList(plates = [], platesList) {
     .join("");
 }
 
+function toggleDone(e) {
+  if (!e.target.matches("input")) return;
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[items].done;
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
 addItems.addEventListener("submit", addItem);
+itemsList.addEventListener("click", toggleDone);
 populateList(items, itemsList);
